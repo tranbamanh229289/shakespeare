9d9a75f6779d4c87d80c8414c831fcb963f57aa9
manhtb <manhtb@moonlab.asia>
2024-03-23 15:38:07 +0700

    update

---
+++ contracts/chainlink/contracts/oracle/DataFeed.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract DataFeed {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function getLastestPrice() external view returns(uint80, int256) {
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+        return (roundId, answer);
+    }
+}
